93b52e4d6b2d49d1cc0b011a0a826c731efd4a96
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-23 18:49:25 +0200

    Add files via upload

---
+++ UnitTest3.3D.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab3.3D/Lab3.3D/Fraction_Public.h"
+#include "../Lab3.3D/Lab3.3D/Fraction_Public.cpp"
+#include "../Lab3.3D/Lab3.3D/Pair.h"
+#include "../Lab3.3D/Lab3.3D/Pair.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest33D
+{
+	TEST_CLASS(UnitTest33D)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Fraction_Public a(1, 5), b(1, 5);
+			Assert::AreEqual(a == b, true);
+		}
+	};
+}
